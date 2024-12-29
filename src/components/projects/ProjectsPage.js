3c90e3b5d9c995708f015abe8359b708e34bdbbc
sarok <140957347+sarok86@users.noreply.github.com>
2024-12-29 00:37:33 -0800

    first commit

--- src/components/projects/ProjectsPage.js
+++ src/components/projects/ProjectsPage.js
@@ -1,38 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-// import project from "@/app/image/project.jpg";
+import project1 from "@/app/image/project1.jpg";
+import project2 from "@/app/image/project2.jpg";
+import project3 from "@/app/image/project3.jpg";
 
 export default function ProjectsPage() {
   return (
-    <div className="w-full absolute  flex flex-col justify-center items-start text-start ">
-      <h1 className="text-7xl my-4 text-[gray]">Send Message</h1>
+    <div className="w-full absolute  flex flex-col justify-center items-start text-start px-12 overflow-hidden">
+      <h1 className="text-7xl my-4 text-[gray]">my works</h1>
 
-      <div className="w-full grid grid-cols-2 gap-2">
-        <Link href="/">
-          <Image
-            // src={project}
-            width={400}
-            height={400}
-            alt="Picture of the author"
-          />
-        </Link>
-        <Link href="/">
-          <Image
-            // src={project}
-            width={400}
-            height={400}
-            alt="Picture of the author"
-          />
-        </Link>
-        <Link href="/">
-          <Image
-            // src={project}
-            width={400}
-            height={400}
-            alt="Picture of the author"
-          />
-        </Link>
+      <div className="w-full  grid grid-flow-row-dense grid-cols-3 grid-rows-3 place-content-center place-items-center ">
+        <div className="w-full col-span-2 ">
+          <Link href="/">
+            <Image
+              src={project1}
+              alt="Picture of the author"
+              className="w-full h-90 p-2"
+            />
+          </Link>
+        </div>
+        <div className="w-full col-span-2">
+          <Link href="/">
+            <Image
+              src={project2}
+              alt="Picture of the author"
+              className="w-full h-90 p-2"
+            />
+          </Link>
+        </div>
+        <div className="w-full col-span-1 row-span-2">
+          <Link href="/">
+            <Image
+              src={project3}
+              alt="Picture of the author"
+              className=" h-[500px] p-2"
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/components/resume/ResumePage.js
+++ src/components/resume/ResumePage.js
@@ -1,58 +1,91 @@
 "use client";
 import { Stack } from "@mui/material";
-import { BsSpeedometer2 } from "react-icons/bs";
-import { Gauge } from "@mui/x-charts/Gauge";
+import { SlSpeedometer } from "react-icons/sl";
+import { Chart as ChartJS } from "chart.js";
+import { Bar } from "react-chartjs-2";
 
 function ResumePage() {
   return (
-    <div className="w-full absolute  flex flex-col justify-center items-start text-start ">
-      <h1>Resume</h1>
-      <div className="flex my-4">
-        <span className="text-3xl text-[blue]">10 years in design</span>
-        <span className="text-3xl  text-white m-2"> / </span>
-        <span className="text-3xl text-[blue]">6 years in coding</span>
-        <span className="text-3xl  text-white m-2"> / </span>
-        <span className="text-3xl text-[blue]">master`s degree</span>
-        <p className="text-2xl text-[bold]">
+    <div className="w-full absolute  flex flex-col justify-center items-start text-start px-12 ">
+      <h1 className="text-7xl mt-8 text-[gray]">Resume</h1>
+      <div className=" my-2">
+        <div className="flex my-4 items-center text-center">
+          <span className="text-3xl text-[red]">10 years in design</span>
+          <span className="text-3xl  text-[gray] m-2"> / </span>
+          <span className="text-3xl text-[red]">6 years in coding</span>
+          <span className="text-3xl  text-[gray] m-2"> / </span>
+          <span className="text-3xl text-[red]">master`s degree</span>
+        </div>
+        <p className="text-2xl text-[gray]">
           Web Developer with 10 years of experience in designing and developing
           user interfaces, testing, debugging, and training staff within
           eCommerce technologies. Proven ability in optimizing web functionality
           that improve data retrieval and workflow efficiencies.
         </p>
       </div>
-      <div className="w-full border-2 bg-white my-12"></div>
-      <div>
-        <h1>
-          <BsSpeedometer2 />
+      <div className="w-full h-2 border-4 bg-[red] my-12"></div>
+
+      <div className="w-full">
+        <h1 className="w-full flex items-center text-center text-6xl mt-8 text-[gray]">
+          <SlSpeedometer className="text-5xl text-[red] m-3" />
           Skills
         </h1>
-        <Stack
-          direction={{ xs: "column", md: "row" }}
-          spacing={{ xs: 1, md: 3 }}
-        >
-          <Gauge width={100} height={100} value={50} />
-          <Gauge
-            width={100}
-            height={100}
-            value={50}
-            valueMin={10}
-            valueMax={60}
-          />
-          <Gauge
-            width={100}
-            height={100}
-            value={50}
-            valueMin={10}
-            valueMax={60}
-          />
-          <Gauge
-            width={100}
-            height={100}
-            value={50}
-            valueMin={10}
-            valueMax={60}
-          />
-        </Stack>
+        <div className="w-full text-[gray] px-12 mt-3">
+          <div className="my-8">
+            <div className="w-full flex justify-between text-2xl">
+              <p>web development</p>
+              <p>90%</p>
+            </div>
+            <div className="w-full h-2  bg-[gray] ">
+              <div className="w-[90%] h-2  border-4 border-[red] bg-[red]"></div>
+            </div>
+          </div>
+          <div className="my-8">
+            <div className="w-full flex justify-between text-2xl">
+              <p>html</p>
+              <p>100%</p>
+            </div>
+            <div className="w-full h-2  bg-[gray] ">
+              <div className="w-[100%] h-2  border-4 border-[red] bg-[red]"></div>
+            </div>
+          </div>
+          <div className="my-8">
+            <div className="w-full flex justify-between text-2xl">
+              <p>css</p>
+              <p>100%</p>
+            </div>
+            <div className="w-full h-2  bg-[gray] ">
+              <div className="w-[100%] h-2  border-4 border-[red] bg-[red]"></div>
+            </div>
+          </div>
+          <div className="my-8">
+            <div className="w-full flex justify-between text-2xl">
+              <p>js</p>
+              <p>95%</p>
+            </div>
+            <div className="w-full h-2  bg-[gray] ">
+              <div className="w-[95%] h-2  border-4 border-[red] bg-[red]"></div>
+            </div>
+          </div>
+          <div className="my-8">
+            <div className="w-full flex justify-between text-2xl">
+              <p>react</p>
+              <p>90%</p>
+            </div>
+            <div className="w-full h-2  bg-[gray] ">
+              <div className="w-[90%] h-2  border-4 border-[red] bg-[red]"></div>
+            </div>
+          </div>
+          <div className="my-8">
+            <div className="w-full flex justify-between text-2xl">
+              <p>next</p>
+              <p>85%</p>
+            </div>
+            <div className="w-full h-2  bg-[gray] ">
+              <div className="w-[85%] h-2  border-4 border-[red] bg-[red]"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
